5cd67a3f3a710b2e6c382ec86e5bcf72e05a1854
Josh Tilles <josh@signafire.com>
2016-01-17 19:04:11 -0500

    Strip out a ton of stuff for IntelliJ's sake.

--- project.clj
+++ project.clj
@@ -6,23 +6,5 @@
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
                                   [org.clojure/test.check "0.9.0"]
-                                  [potemkin "0.4.1"]]
-                   :plugins [[codox "0.8.8"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC1"]]}}
-
-  :aliases {"all" ["with-profile" "dev:dev,1.8"]
-            "deploy" ["do" "clean," "deploy" "clojars"]
-            "test" ["do" "clean," "test"]}
-
-  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
-
-  :lein-release {:deploy-via :shell
-                 :shell ["lein" "deploy"]}
-
-  :auto-clean false
-
-  :codox {;; :src-uri-mapping {#"target/generated/src/clj" #(str "src/cljx/" % "x")}
-          :src-dir-uri "http://github.com/plumatic/schema/blob/master/"
-          :src-linenum-anchor-prefix "L"}
-
-  :signing {:gpg-key "66E0BF75"})
+                                  [potemkin "0.4.1"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC1"]]}})
