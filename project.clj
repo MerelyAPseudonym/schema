4b418e224400233cffab82f83a662653e948471a
Josh Tilles <josh@signafire.com>
2016-01-17 19:06:16 -0500

    Add some debugging libs for my convenience.

--- project.clj
+++ project.clj
@@ -6,5 +6,9 @@
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
                                   [org.clojure/test.check "0.9.0"]
+                                  [org.clojure/tools.logging "0.3.1"]
+                                  [org.clojure/tools.namespace "0.2.11"]
+                                  [spootnik/unilog "0.7.13"]
+                                  [com.taoensso/timbre "4.2.1"]
                                   [potemkin "0.4.1"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC1"]]}})
